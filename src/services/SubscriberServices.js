c8c8b2721a3c582b712b8b7f303866233379a8a4
Anuradha Adhikari <adhikarianuradha336@gmail.com>
2024-12-01 21:10:45 +0530

    added url

--- src/services/SubscriberServices.js
+++ src/services/SubscriberServices.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 // const REST_API_BASE_URL="http://localhost:3030";
+const REST_API_BASE_URL="https://subscriber-registration-backend-1.onrender.com/";
 
 export const createSubscriber = (data) => {
     return axios.post(`${REST_API_BASE_URL}/subscriber`, data);
