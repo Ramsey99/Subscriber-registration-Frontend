0d7448815690a2f6debd97ce1eb8b39e957879aa
Anuradha Adhikari <adhikarianuradha336@gmail.com>
2024-12-02 11:31:08 +0530

    running on local

--- src/services/SubscriberServices.js
+++ src/services/SubscriberServices.js
@@ -1,13 +1,6 @@
 import axios from 'axios';
+const REST_API_BASE_URL="http://localhost:5000";
 
-// Base URL for your backend
-const REST_API_BASE_URL = "https://subscriber-registration-backend-1.onrender.com/api";
-
-/**
- * Function to create a subscriber
- * @param {Object} data - Subscriber data to be sent to the backend
- * @returns {Promise} Axios POST request promise
- */
 export const createSubscriber = (data) => {
     return axios.post(`${REST_API_BASE_URL}/subscribers`, data);
-};
+}
